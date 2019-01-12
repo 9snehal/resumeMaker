af1c5241d8158536bd298892549f08eee482cfd4
Snehal Narkar <snehalvnarkar@gmail.com>
2019-01-12 12:48:55 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function results(){
+var name = document.getElementById('name').value;
+var email=document.getElementById('email').value;
+var add=document.getElementById('add').value;
+var mob=document.getElementById('Phone').value;
+document.write("<h1>Your Resume</h1>");
+document.write("<h2>Name:</h2>"+ name + "<br/>");
+document.write("<h2>Email:</h2>"+ email + "<br/>");
+document.write("<h2>Address:</h2>"+ add + "<br/>");
+document.write("<h2>Mobile No:></h2>"+ mob + "<br/>");
+}
