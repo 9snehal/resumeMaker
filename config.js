d24caacc1ae265b58be72c259ea6ffdb3d0c054a
Snehal Narkar <snehalvnarkar@gmail.com>
2019-01-19 12:16:27 +0530

    css add

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+   apiKey: "AIzaSyC3JMz6bOK-Fs17u5JhqtKIocCX2Pq1raY",
+   authDomain: "resume-20f77.firebaseapp.com",
+   databaseURL: "https://resume-20f77.firebaseio.com",
+   projectId: "resume-20f77",
+   storageBucket: "resume-20f77.appspot.com",
+   messagingSenderId: "273935609010"
+ };
+ firebase.initializeApp(config);
